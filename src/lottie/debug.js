1e6bc69b9c3d48403ec883a8400355afad964fd4
蒋瑶 <landn172@gmail.com>
2018-06-06 17:39:01 +0800

    feat: 添加debug耗时

--- src/lottie/debug.js
+++ src/lottie/debug.js
@@ -12,7 +12,9 @@ export function proxyCtx(ctx) {
     if (typeof oldMethod === 'function') {
       Object.defineProperty(ctx, method, {
         value: function (...args) {
-          if (enableProxyLog) { console.log.call(null, [`ctx.${method}(`].concat(args.join(',')).concat(')').join('')); }
+          if (enableProxyLog) {
+            console.log.call(null, [`ctx.${method}(`].concat(args.join(',')).concat(')').join(''));
+          }
           return oldMethod.apply(ctx, args);
         }
       });
@@ -29,3 +31,23 @@ export function proxyCtx(ctx) {
     });
   }
 }
+
+function debugElapsedTimeFactory() {
+  const nameLog = {};
+  return {
+    start(groupName) {
+      nameLog[groupName] = +new Date();
+    },
+    end(groupName) {
+      const value = nameLog[groupName];
+      if (typeof value === 'undefined') {
+        throw new Error(`debugElapsedTimeFactory start(${groupName}) is not invoke before end()`);
+      }
+      console.log(`debug:${groupName}`, +new Date() - value + 'ms');
+      delete nameLog[groupName];
+    }
+  };
+}
+
+// 打印耗时
+export const debug = debugElapsedTimeFactory();
