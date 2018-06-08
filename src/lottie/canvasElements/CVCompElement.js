ddcf1c7b7311fec2d2c3a7c911313fe80446351f
蒋瑶 <landn172@gmail.com>
2018-06-08 10:28:23 +0800

    fix: 调整CVCompElement实现方式

--- src/lottie/canvasElements/CVCompElement.js
+++ src/lottie/canvasElements/CVCompElement.js
@@ -1,45 +1,48 @@
 import Mixin from '../utils/mixin';
-import CanvasRenderer from '../renderers/CanvasRenderer';
 import ICompElement from '../elements/CompElement';
 import CVBaseElement from './CVBaseElement';
 import PropertyFactory from '../utils/PropertyFactory';
 
-class CVCompElement extends Mixin(CanvasRenderer, ICompElement, CVBaseElement) {
-  constructor(data, globalData, comp) {
-    super();
-    this.completeLayers = false;
-    this.layers = data.layers || [];
-    this.pendingElements = [];
-    this.elements = Array.apply(null, {
-      length: this.layers.length
-    });
-    this.initElement(data, globalData, comp);
-    this.tm = data.tm ? PropertyFactory.getProp(this, data.tm, 0, globalData.frameRate, this) : {
-      _placeholder: true
-    };
-  }
+let CVCompElementClass;
 
-  renderInnerContent() {
-    let i;
-    let len = this.layers.length;
-    for (i = len - 1; i >= 0; i -= 1) {
-      if (this.completeLayers || this.elements[i]) {
-        this.elements[i].renderFrame();
+export default (CanvasRenderer) => {
+  if (CVCompElementClass) return CVCompElementClass;
+  CVCompElementClass = class CVCompElement extends Mixin(CanvasRenderer, ICompElement, CVBaseElement) {
+    constructor(data, globalData, comp) {
+      super();
+      this.completeLayers = false;
+      this.layers = data.layers || [];
+      this.pendingElements = [];
+      this.elements = Array.apply(null, {
+        length: this.layers.length
+      });
+      this.initElement(data, globalData, comp);
+      this.tm = data.tm ? PropertyFactory.getProp(this, data.tm, 0, globalData.frameRate, this) : {
+        _placeholder: true
+      };
+    }
+
+    renderInnerContent() {
+      let i;
+      let len = this.layers.length;
+      for (i = len - 1; i >= 0; i -= 1) {
+        if (this.completeLayers || this.elements[i]) {
+          this.elements[i].renderFrame();
+        }
       }
     }
-  }
 
-  destroy() {
-    let i;
-    let len = this.layers.length;
-    for (i = len - 1; i >= 0; i -= 1) {
-      if (this.elements[i]) {
-        this.elements[i].destroy();
+    destroy() {
+      let i;
+      let len = this.layers.length;
+      for (i = len - 1; i >= 0; i -= 1) {
+        if (this.elements[i]) {
+          this.elements[i].destroy();
+        }
       }
+      this.layers = null;
+      this.elements = null;
     }
-    this.layers = null;
-    this.elements = null;
-  }
-}
-
-export default CVCompElement;
+  };
+  return CVCompElementClass;
+};
