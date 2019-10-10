b4d522cfb0d009c9966f552d33aee5d19e7c7a2c
蒋瑶 <landn172@gmail.com>
2019-10-10 16:46:07 +0800

    feat: add uni-app example

---
+++ uniapp-example/src/pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <canvas
+    id="test-canvas"
+    canvas-id="test-canvas"
+    class="test-canvas"
+    binderror="error"
+  ></canvas>
+</template>
+
+<script>
+import lottie from 'lottie-miniapp';
+
+export default {
+  data() {
+    return {
+      title: 'Hello'
+    };
+  },
+  onReady() {
+    const canvasContext = wx.createCanvasContext('test-canvas');
+    canvasContext.canvas = {
+      width: 200,
+      height: 200
+    };
+
+    lottie.loadAnimation({
+      renderer: 'canvas', // 只支持canvas
+      loop: true,
+      autoplay: true,
+      path: `https://github.com/landn172/lottie-miniapp/files/3711191/home_animete.-.txt`,
+      rendererSettings: {
+        context: canvasContext,
+        clearCanvas: true
+      }
+    });
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.test-canvas {
+  height: 200px;
+  width: 200px;
+}
+</style>
