2e505830a8c104920e3f745dce3a09409c88a8d6
landn172 <landn172@gmail.com>
2020-09-24 11:43:27 +0800

    fix: canvas type=2d replay not active

--- example/pages/index/index.js
+++ example/pages/index/index.js
@@ -1,101 +1,119 @@
-import lottie from '../../lottie/index';
+import lottie from "../../lottie/index";
 
 Page({
   data: {
-    currentValue: '',
+    currentValue: "",
     json: [
       {
-        name: '本地动画',
-        value: "../../data/data1.json.js"
+        name: "本地动画",
+        value: "../../data/data1.json.js",
       },
       {
-        name: '字母B',
-        value: "https://github.com/landn172/lottie-miniapp/files/3146707/data.txt"
+        name: "字母B",
+        value:
+          "https://github.com/landn172/lottie-miniapp/files/3146707/data.txt",
       },
       {
-        name: '小树',
-        value: "https://github.com/landn172/lottie-miniapp/files/3146709/data2.txt"
+        name: "小树",
+        value:
+          "https://github.com/landn172/lottie-miniapp/files/3146709/data2.txt",
       },
       {
-        name: '字体',
-        value: "https://github.com/landn172/lottie-miniapp/files/3147067/font.txt"
-      }
+        name: "字体",
+        value:
+          "https://github.com/landn172/lottie-miniapp/files/3147067/font.txt",
+      },
     ],
   },
   error(e) {
-    console.error(e)
+    console.error(e);
   },
   onChange(e) {
-    const idx = e.detail.value * 1
-    const value = this.data.json[idx]
+    const idx = e.detail.value * 1;
+    const value = this.data.json[idx];
     this.setData({
-      currentValue: value.name
-    })
-    this.playLottie(value.value)
+      currentValue: value.name,
+    });
+    this.playLottie(value.value);
   },
   playLottie(lottieData) {
-    const ctx = wx.createCanvasContext('test-canvas')
+    const ctx = wx.createCanvasContext("test-canvas");
     const l = lottieTest(ctx, lottieData);
-    l.addEventListener('loopComplete', ()=>{
-      console.log('loopComplete')
-    })
+    this.lottie = l;
+    l.addEventListener("loopComplete", () => {
+      console.log("loopComplete");
+    });
+    l.addEventListener("config_ready", () => {
+      console.log("config_ready");
+    });
+    l.addEventListener("data_failed", () => {
+      console.log("data_failed");
+    });
+    l.addEventListener("loaded_images", () => {
+      console.log("loaded_images");
+    });
   },
-  onReady: function() {
-    const lottieData = this.options.path
+  onReady: function () {
+    const lottieData = this.options.path;
     if (lottieData) {
-      this.playLottie(lottieData)
+      this.playLottie(lottieData);
     }
   },
-})
+  onUnload: function () {
+    if(this.lottie) {
+      this.lottie.destroy()
+    }
+  },
+});
 
 function lottieTest(canvasContext, lottieData) {
-  if (!canvasContext || !lottieData) return
+  if (!canvasContext || !lottieData) return;
 
   lottie.destroy();
-  const systemInfo = wx.getSystemInfoSync()
+  const systemInfo = wx.getSystemInfoSync();
 
-  const context = canvasContext
+  const context = canvasContext;
 
   canvasContext.canvas = {
     width: systemInfo.windowWidth,
-    height: systemInfo.windowHeight
-  }
+    height: systemInfo.windowHeight,
+  };
 
-  Object.defineProperty(canvasContext, 'globalAlpha', {
+  Object.defineProperty(canvasContext, "globalAlpha", {
     get() {
-      return this._globalAlpha
+      return this._globalAlpha;
     },
     set(value) {
-      this._globalAlpha = value
-    }
-  })
-  canvasContext.globalAlpha = 1
-  if (typeof lottieData === 'string') {
-    if (lottieData.startsWith('http')) {
+      this._globalAlpha = value;
+    },
+  });
+  canvasContext.globalAlpha = 1;
+  if (typeof lottieData === "string") {
+    if (lottieData.startsWith("http")) {
       return lottie.loadAnimation({
-        renderer: 'canvas',
+        renderer: "canvas",
         loop: true,
         autoplay: true,
         path: lottieData,
         rendererSettings: {
           context: canvasContext,
-          clearCanvas: true
-        }
-      })
+          clearCanvas: true,
+        },
+      });
     }
-    if (lottieData.startsWith('.')) {
-      lottieData = require(lottieData)
+    if (lottieData.startsWith(".")) {
+      lottieData = require(lottieData);
     }
   }
 
   return lottie.loadAnimation({
-    renderer: 'canvas',
+    renderer: "canvas",
     loop: true,
     autoplay: true,
     animationData: lottieData,
     rendererSettings: {
       context: canvasContext,
-      clearCanvas: true
-    }
-  })
+      clearCanvas: true,
+    },
+  });
 }
